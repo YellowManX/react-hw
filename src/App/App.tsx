102b45917b08faae0141eb1e3363b3d9cf8e08c8
YellowManX <106083752+YellowManX@users.noreply.github.com>
2023-02-27 15:48:04 +0600

    first commit

---
+++ src/App/App.tsx
@@ -0,0 +1,85 @@
+// App - компонент
+import React, { Component } from "react";
+import { TodoList, Header } from "../components";
+import { ITodo } from "../types";
+
+// JSX - компоненты
+// JSX - элементы
+// const App = () => {
+
+//     return (
+//         <div>
+//             <Header title="Todo App" />
+//             <TodoList />
+//         </div>
+//     )
+// }
+
+// export default App;
+
+// Property drill
+
+interface IAppState {
+  todos: ITodo[];
+}
+
+export default class App extends Component<{}, IAppState> {
+  state = {
+    todos: [
+      { id: 1, text: "Learn React", done: true, important: true },
+      { id: 2, text: "Drink Water", done: false, important: false },
+      { id: 3, text: "Drink Soda", done: false, important: false },
+    ],
+  };
+
+  onChangeStateTodosImportant = (id: number) => {
+    this.setState((state) => {
+      const todoIdx = state.todos.findIndex((item) => item.id === id);
+      const newTodo = {
+        ...state.todos[todoIdx],
+        important: !state.todos[todoIdx].important,
+      };
+
+      // 3. создали новый массив с обновленной таской
+      const before = state.todos.slice(0, todoIdx);
+      const after = state.todos.slice(todoIdx + 1);
+
+      return {
+        todos: [...before, newTodo, ...after],
+      };
+    });
+  };
+
+  onChangeStateTodos = (id: number) => {
+    this.setState((state) => {
+      // 1. нашли индекс элемента на который кликнули
+      const todoIdx = state.todos.findIndex((item) => item.id === id);
+      // 2. создали копию с противоположным значением
+      const newTodo = {
+        ...state.todos[todoIdx],
+        done: !state.todos[todoIdx].done,
+      };
+
+      // 3. создали новый массив с обновленной таской
+      const before = state.todos.slice(0, todoIdx);
+      const after = state.todos.slice(todoIdx + 1);
+
+      return {
+        todos: [...before, newTodo, ...after],
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Header title="Todo App" />
+        <TodoList
+          todos={this.state.todos}
+          onDone={this.onChangeStateTodos}
+          onImportant={this.onChangeStateTodosImportant}
+        />
+      </div>
+    );
+  }
+}
---
+++ src/components/TodoListItem/TodoListItem.tsx
@@ -0,0 +1,86 @@
+// const TodoListItem = (props: { text: string }) => {
+
+//     return (
+//         <li>{props.text}</li>
+//     )
+// }
+
+// export default TodoListItem;
+
+import React, { Component } from "react";
+import { ITodo } from "../../types";
+import "./TodoListItem.css";
+
+// Mutable(мутабельность) - изменение
+// state - незименяемый (inmutable)
+
+interface ITodoListItemState {
+  done: boolean;
+  important: boolean;
+}
+
+interface ITodoListItemProps extends ITodo {
+  onDoneClick: () => void;
+  onImportantClick: () => void;
+}
+
+export default class TodoListItem extends Component<
+  ITodoListItemProps,
+  ITodoListItemState
+> {
+  state = {
+    done: false,
+    important: false,
+  };
+
+  //   onDoneClick = () => {
+  //     console.log("click");
+  //     // this.state.done = !this.state.done;
+  //     // 1. Передаем в setState объект с НОВЫМИ значениями state
+  //     // this.setState({ done: !this.state.done })
+  //     // 2. В setState передаем колбэк в который приходит актуальный state.
+  //     // Используем тогда когда меняем значение, которое ЗАВИСИТ от предыдущего
+  //     this.setState((state) => {
+  //       return {
+  //         done: !state.done,
+  //       };
+  //     });
+  //   };
+
+  // onImportantClick = () => {
+  //     this.setState((state) => {
+  //         return {
+  //             important: !state.important
+  //         }
+  //     })
+  // }
+
+  onChangeState = (nameState: "done" | "important") => {
+    //@ts-ignore
+    this.setState((state) => {
+      //@ts-ignore
+      return {
+        [nameState]: !state[nameState],
+      };
+    });
+  };
+
+  render() {
+    const { text, done, important } = this.props;
+
+    let classNames = "";
+    if (done) {
+      classNames += " done";
+    }
+    if (important) {
+      classNames += " important";
+    }
+    return (
+      <li className={classNames}>
+        {text}
+        <button onClick={this.props.onDoneClick}>Done</button>
+        <button onClick={this.props.onImportantClick}>Important</button>
+      </li>
+    );
+  }
+}
